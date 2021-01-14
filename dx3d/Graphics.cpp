d8aa87a6cf295697d10e9757919f0bf0f79082da
nipingbo <nipingbo@gmail.com>
2021-01-14 20:55:35 +0800

    Clear backbuffer

--- dx3d/Graphics.cpp
+++ dx3d/Graphics.cpp
@@ -35,10 +35,25 @@ Graphics::Graphics(HWND hWnd)
 		nullptr,
 		&pContext
 		);
+	//gain access to texture subresource in swap chain (back buffer)
+	ID3D11Resource* pBackBuffer = nullptr;
+	pSwap->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&pBackBuffer));
+	pDevice->CreateRenderTargetView(
+		pBackBuffer,
+		nullptr,
+		&pTarget
+	);
+	pBackBuffer->Release();
+
+
 }
 
 Graphics::~Graphics()
 {
+	if (pTarget != nullptr)
+	{
+		pTarget->Release();
+	}
 	if (pContext != nullptr)
 	{
 		pContext->Release();
@@ -56,4 +71,10 @@ Graphics::~Graphics()
 void Graphics::EndFrame()
 {
 	pSwap->Present(1u, 0u);
+}
+
+void Graphics::ClearBuffer(float red, float green, float blue) noexcept
+{
+	const float color[] = { red, green, blue, 1.0f };
+	pContext->ClearRenderTargetView(pTarget, color);
 }
