cf1ea6832007d2f0c9590db0a71baea4535cb7d5
nipingbo <nipingbo@gmail.com>
2021-01-15 13:22:51 +0800

    Add class for dxgi debug info

--- dx3d/Graphics.h
+++ dx3d/Graphics.h
@@ -2,6 +2,8 @@
 #include "NirWin.h"
 #include "NirException.h"
 #include <d3d11.h>
+#include <vector>
+#include "DxgiInfoManager.h"
 class Graphics
 {
 public:
@@ -12,20 +14,24 @@ public:
 	class HrException : public Exception
 	{
 	public:
-		HrException(int line, const char* file, HRESULT hr) noexcept;
+		HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs = {}) noexcept;
 		const char* what() const noexcept override;
 		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
 		std::string GetErrorString() const noexcept;
 		std::string GetErrorDescription() const noexcept;
+		std::string GetErrorInfo() const noexcept;
 	private:
 		HRESULT hr;
+		std::string info;
 	};
 	class DeviceRemovedException : public HrException
 	{
 		using HrException::HrException;
 	public:
 		const char* GetType() const noexcept override;
+	private:
+		std::string reason;
 	};
 public:
 	Graphics(HWND hWnd);
@@ -35,6 +41,9 @@ public:
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue) noexcept;
 private:
+#ifndef NDEBUG
+	DxgiInfoManager infoManager;
+#endif
 	ID3D11Device* pDevice = nullptr;
 	IDXGISwapChain* pSwap = nullptr;
 	ID3D11DeviceContext* pContext = nullptr;
