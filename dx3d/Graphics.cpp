36c9c1052163ff52f8c16f388653af944f6b346b
nipingbo <nipingbo@gmail.com>
2021-01-20 14:37:10 +0800

    contantbuffer and rotate

--- dx3d/Graphics.cpp
+++ dx3d/Graphics.cpp
@@ -2,6 +2,7 @@
 #include "dxerr.h"
 #include <sstream>
 #include <d3dcompiler.h>
+#include <cmath>
 
 namespace wrl = Microsoft::WRL;
 
@@ -96,7 +97,7 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 	pContext->ClearRenderTargetView(pTarget.Get(), color);
 }
 
-void Graphics::DrawTestTriangle()
+void Graphics::DrawTestTriangle(float angle)
 {
 	namespace wrl = Microsoft::WRL;
 	HRESULT hr;
@@ -172,6 +173,40 @@ void Graphics::DrawTestTriangle()
 
 	// bind index buffer
 	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
+
+	//create constant buffer for tranformation matrix
+	struct ConstantBuffer {
+		struct
+		{
+			float element[4][4];
+		} transformation;
+		  
+	};
+	const ConstantBuffer cb = 
+	{
+		{
+			std::cos(angle),	std::sin(angle),	0.0f,	0.0f,
+			-std::sin(angle),	std::cos(angle),	0.0f,	0.0f,
+			0.0f,				0.0f,				1.0f,	0.0f,
+			0.0f,				0.0f,				0.0f,	1.0f,
+		}
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pConstantBuffer;
+	D3D11_BUFFER_DESC cbd;
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd.Usage = D3D11_USAGE_DYNAMIC;
+	cbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cbd.MiscFlags = 0u;
+	cbd.ByteWidth = sizeof(cb);
+	cbd.StructureByteStride = 0u;
+
+	D3D11_SUBRESOURCE_DATA csd = {};
+	csd.pSysMem = &cb;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&cbd, &csd, &pConstantBuffer));
+	//bind constant buffer to vertex shader
+	pContext->VSSetConstantBuffers(0u, 1u, pConstantBuffer.GetAddressOf());
+
 
 
 	//create pixel shader
--- dx3d/VertexShader.hlsl
+++ dx3d/VertexShader.hlsl
@@ -5,10 +5,15 @@ struct VSOut
 	
 };
 
+cbuffer CBuf
+{
+	matrix transformation;
+};
+
 VSOut main(float2 pos : Position, float3 color : Color) 
 {
 	VSOut vso;
-	vso.pos = float4(pos.x, pos.y, 0.0f, 1.0f);
+	vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f),transformation);
 	vso.color = color;
 	return  vso;
 }
